e68846175615c035c8563a75e5ac5c2a7fe6279f
Nguyen Bao Xuan Truong <omega220@gmail.com>
2017-07-04 14:57:53 +0700

    update

--- bookTable/home.js
+++ bookTable/home.js
@@ -1,29 +1,29 @@
-var application=angular.module('myApplication',[]);
-application.controller('myController',function($scope){
+var application = angular.module('myApplication', []);
+application.controller('myController', function($scope){
 
 	$scope.bookData = [
 		{book: 'data', author: 'tom'},
 		{book: 'history', author: 'anna'}
 	];
 
-	$scope.addRow=function(){
+	$scope.addRow = function() {
 		$scope.bookData.push({
-		book: $scope.inputBook,
-		author: $scope.inputAuthor
+			book: $scope.inputBook,
+			author: $scope.inputAuthor
 		});
 	}
 
-	$scope.removeRow = function(index){
+	$scope.removeRow = function(index) {
 		$scope.bookData.splice(index, 1);
 	};
 
-	$scope.modify = function(tableData){
+	$scope.modify = function(tableData) {
 		$scope.modifyField = true;
 		$scope.viewField = true;
 	};
 
 
-	$scope.update = function(tableData){
+	$scope.update = function(tableData) {
 		$scope.modifyField = false;
 		$scope.viewField = false;
 	};
---
+++ bookTable2/home.js
@@ -0,0 +1,36 @@
+var app = angular.module('addApp', []);
+app.controller('modifyCtrl', function($scope){
+    $scope.bookData= [
+    {name:'java', author:'tom', id:'0' },
+    {name:'cpp', author:'anna', id:'1' }
+    ];
+    
+    $scope.idCounter=2;
+    $scope.editingData = {};
+
+    for (var i = 0, length = $scope.bookData.length; i < length; i++) {
+        $scope.editingData[$scope.bookData[i].id] = false;
+    }
+
+    $scope.modify = function(tableData){
+        $scope.editingData[tableData.id] = true;
+    };
+
+
+    $scope.update = function(tableData){
+        $scope.editingData[tableData.id] = false;
+    };
+
+    $scope.addRow=function(){
+        $scope.bookData.push({
+            name: $scope.inputBook,
+            author: $scope.inputAuthor,
+            id: $scope.idCounter
+        });
+        $scope.idCounter=$scope.idCounter+1;
+    }
+
+    $scope.removeRow = function(index){
+        $scope.bookData.splice(index, 1);
+    };
+});
