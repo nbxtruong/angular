68309efa32ade7e9fdf03b0b31e182caf33d75a9
Nguyen Bao Xuan Truong <omega220@gmail.com>
2017-07-14 09:38:09 +0700

    Create service and use it.

--- bookTable3/home.js
+++ bookTable3/home.js
@@ -1,6 +1,6 @@
 var homeApp = angular.module('homeApp', []);
 
-homeApp.controller('homeCtrl', function ($scope) {
+homeApp.controller('homeCtrl', function ($scope, editingService) {
     $scope.bookData = [
         { name: 'java', author: 'tom', id: '0' },
         { name: 'cpp', author: 'anna', id: '1' }
@@ -9,9 +9,11 @@ homeApp.controller('homeCtrl', function ($scope) {
     $scope.editingData = {};
     length = $scope.bookData.length;
 
-    for (var i = 0; i < length; i++) {
-        $scope.editingData[$scope.bookData[i].id] = false;
-    }
+    // for (var i = 0; i < length; i++) {
+    //     $scope.editingData[$scope.bookData[i].id] = false;
+    // }
+    editingService.editingFunction($scope.bookData, $scope.editingData);
+
 
     $scope.modify = function (tableData) {
         $scope.editingData[tableData.id] = true;
@@ -32,6 +34,14 @@ homeApp.controller('homeCtrl', function ($scope) {
     }
 
     $scope.removeRow = function (index) {
-        $scope.bookData.splice(index, 1);
+        $scope.bookData.splice(index, 2);
     };
+});
+
+homeApp.service('editingService', function () {
+    this.editingFunction = function (bookData, editingData) {
+        for (var i = 0; i < bookData.length; i++) {
+            editingData[bookData[i].id] = false;
+        }
+    }
 });
